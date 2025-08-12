2e9b1664bb5bef70d2ef3f8c2cc04b3409b61431
art8461 <art8461@gmail.com>
2025-08-12 16:09:50 +0300

    Создан проект

---
+++ ImageFeed/ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Artem Kuzmenko on 12.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

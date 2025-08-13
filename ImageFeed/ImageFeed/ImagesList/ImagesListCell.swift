01ab4a8d16c93be41bcb7d8864c88b4eb258691f
art8461 <art8461@gmail.com>
2025-08-13 20:01:34 +0300

    Добавлен градиентная подложка внизу ячейки

--- ImageFeed/ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImageFeed/ImagesList/ImagesListCell.swift
@@ -8,78 +8,102 @@
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
+    
+    // MARK: - Публичные переменные
     static let reuseIdentifier = "ImagesListCell"
     
+    // MARK: - IBOutlet
     @IBOutlet weak var cellImageView: UIImageView!
-    
     @IBOutlet weak var cellTextLabel: UILabel!
-    
     @IBOutlet weak var likeButton: UIButton!
     
+    // MARK: - Приватные переменные
+    private var gradientLayer: CAGradientLayer?
+    
+    // MARK: - Публичные методы
     override func awakeFromNib() {
         super.awakeFromNib()
-
+        setupGradient()
+        
         contentView.layer.masksToBounds = true
-        // Прозрачный фон ячейки
         backgroundColor = .clear
         contentView.backgroundColor = .clear
-        selectionStyle = .none // Отключаем выделение
+        selectionStyle = .none
         
-        // Настраиваем внешний вид ImageView
+        // ImageView
         cellImageView.contentMode = .scaleAspectFill
         cellImageView.clipsToBounds = true
         cellImageView.layer.cornerRadius = 16
-        
-        // Отключение констрейнов у ImageView
         cellImageView.translatesAutoresizingMaskIntoConstraints = false
-        // Констрейны для ImageView
         NSLayoutConstraint.activate([
             cellImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 4),
-            cellImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor,constant: 16),
+            cellImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
             cellImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
             cellImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -4)
         ])
         
-        // Настройка текста
+        // Label
         cellTextLabel.textColor = UIColor.white
         cellTextLabel.font = UIFont(name: "SFProText-Regular", size: 13)
         cellTextLabel.numberOfLines = 2
-        // Отключение констрейнов у cellTextLabel
         cellTextLabel.translatesAutoresizingMaskIntoConstraints = false
-        // Констрейны для cellTextLabel
         NSLayoutConstraint.activate([
             cellTextLabel.leadingAnchor.constraint(equalTo: cellImageView.leadingAnchor, constant: 8),
             cellTextLabel.bottomAnchor.constraint(equalTo: cellImageView.bottomAnchor, constant: -8),
             cellTextLabel.trailingAnchor.constraint(lessThanOrEqualTo: cellImageView.trailingAnchor, constant: -8)
         ])
         
-        // Настраиваем кнопки лайка
+        // Like Button
         likeButton.setImage(UIImage(named: "NoActive"), for: .normal)
         likeButton.setImage(UIImage(named: "Active"), for: .selected)
         likeButton.setTitle("", for: .normal)
         likeButton.setTitle("", for: .selected)
-        
         likeButton.tintColor = .clear
-
-        // Отключение констрейнов у likeButton
         likeButton.translatesAutoresizingMaskIntoConstraints = false
-        // Констрейны для likeButton
         NSLayoutConstraint.activate([
-                likeButton.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 0),
-                likeButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
-                likeButton.widthAnchor.constraint(equalToConstant: 44),
-                likeButton.heightAnchor.constraint(equalToConstant: 44)
-            ])
+            likeButton.topAnchor.constraint(equalTo: contentView.topAnchor),
+            likeButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            likeButton.widthAnchor.constraint(equalToConstant: 44),
+            likeButton.heightAnchor.constraint(equalToConstant: 44)
+        ])
     }
     
-    @IBAction private func didTapLike(_ sender: Any) {
-        likeButton.isSelected.toggle() // переключаем состояние
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        updateGradientFrame()
     }
     
-    // Метод для конфигурации ячейки
     func configure(with image: UIImage?, text: String, isLiked: Bool) {
         cellImageView.image = image
         cellTextLabel.text = text
         likeButton.isSelected = isLiked
+    }
+    
+    // MARK: - Приватные методы
+    private func setupGradient() {
+        let gradient = CAGradientLayer()
+        gradient.colors = [
+            UIColor(red: 26/255, green: 27/255, blue: 34/255, alpha: 1.0).cgColor, // низ — сплошной
+            UIColor(red: 26/255, green: 27/255, blue: 34/255, alpha: 0.0).cgColor  // верх — прозрачный
+        ]
+        gradient.startPoint = CGPoint(x: 0.5, y: 1.0) // снизу
+        gradient.endPoint = CGPoint(x: 0.5, y: 0.0)   // вверх
+        
+        cellImageView.layer.addSublayer(gradient)
+        self.gradientLayer = gradient
+    }
+    
+    private func updateGradientFrame() {
+        gradientLayer?.frame = CGRect(
+            x: 0,
+            y: cellImageView.bounds.height - 30,
+            width: cellImageView.bounds.width,
+            height: 30
+        )
+    }
+    
+    // MARK: - IBAction
+    @IBAction private func didTapLike(_ sender: Any) {
+        likeButton.isSelected.toggle()
     }
 }

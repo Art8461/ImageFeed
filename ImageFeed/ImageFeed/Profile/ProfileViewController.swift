5e53c29e91d78fa5c20eaa223bc50849f88089d5
art8461 <art8461@gmail.com>
2025-08-25 10:40:15 +0300

    Экран профиля переписан в код

--- ImageFeed/ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/ImageFeed/Profile/ProfileViewController.swift
@@ -9,73 +9,105 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
-    @IBOutlet weak var photoProfile: UIImageView!
-    @IBOutlet weak var userName: UILabel!
-    @IBOutlet weak var userNickName: UILabel!
-    @IBOutlet weak var descriptionProfile: UILabel!
-    @IBOutlet weak var exitButton: UIButton!
+    // MARK: - UI Элементы
+    private let photoProfile: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage(systemName: "person.crop.circle.fill")
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 35
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
     
-    @IBAction func exitButtonAction(_ sender: Any) {
-        dismiss(animated: true)
-    }
+    private let userName: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont(name: "SFProText-Bold", size: 23)
+        label.text = "Имя пользователя"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let userNickName: UILabel = {
+        let label = UILabel()
+        label.textColor = .gray
+        label.font = UIFont(name: "SFProText-Regular", size: 13)
+        label.text = "@nickname"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let descriptionProfile: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont(name: "SFProText-Regular", size: 13)
+        label.numberOfLines = 0
+        label.text = "Описание профиля"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let exitButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "ipad.and.arrow.forward"), for: .normal)
+        button.tintColor = .systemRed
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     
     // MARK: - Публичные методы
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupUI()
+        setupConstraints()
+    }
+    
+    // MARK: - Настройка интерфейса
+    private func setupUI() {
         view.backgroundColor = UIColor(red: 26/255, green: 27/255, blue: 34/255, alpha: 1.0)
-
-        // PhotoProfile
-        photoProfile.contentMode = .scaleAspectFill
-        photoProfile.image = UIImage(systemName: "person.crop.circle.fill")
-        photoProfile.clipsToBounds = true
-        photoProfile.layer.cornerRadius = 35
-        photoProfile.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(photoProfile)
+        view.addSubview(userName)
+        view.addSubview(userNickName)
+        view.addSubview(descriptionProfile)
+        view.addSubview(exitButton)
         
+        exitButton.addTarget(self, action: #selector(exitButtonTapped), for: .touchUpInside)
+        
+    }
+    
+    // MARK: - Констрейнты
+    private func setupConstraints() {
         NSLayoutConstraint.activate([
+            // Фото профиля
             photoProfile.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
-            photoProfile.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            photoProfile.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
             photoProfile.widthAnchor.constraint(equalToConstant: 70),
-            photoProfile.heightAnchor.constraint(equalToConstant: 70)
-        ])
-        
-        // UserName
-        userName.textColor = UIColor.white
-        userName.font = UIFont(name: "SFProText-Bold", size: 23)
-        userName.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
+            photoProfile.heightAnchor.constraint(equalToConstant: 70),
+            
+            // Имя пользователя
             userName.topAnchor.constraint(equalTo: photoProfile.bottomAnchor, constant: 8),
-            userName.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-        ])
-        
-        // User NickName
-        userNickName.textColor = UIColor.gray
-        userNickName.font = UIFont(name: "SFProText-Regular", size: 13)
-        userNickName.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
+            userName.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            
+            // Никнейм
             userNickName.topAnchor.constraint(equalTo: userName.bottomAnchor, constant: 8),
-            userNickName.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-        ])
-        
-        // Description Profile
-        descriptionProfile.textColor = UIColor.white
-        descriptionProfile.font = UIFont(name: "SFProText-Regular", size: 13)
-        descriptionProfile.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
+            userNickName.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            
+            // Описание профиля
             descriptionProfile.topAnchor.constraint(equalTo: userNickName.bottomAnchor, constant: 8),
-            descriptionProfile.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            descriptionProfile.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
-        ])
-        // Exit Button
-        exitButton.setImage(UIImage(systemName: "ipad.and.arrow.forward"), for: .normal)
-        exitButton.tintColor = .systemRed
-        exitButton.setTitle("", for: .normal)
-        exitButton.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
+            descriptionProfile.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            descriptionProfile.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            // Кнопка выхода
             exitButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 45),
-            exitButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            exitButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
             exitButton.widthAnchor.constraint(equalToConstant: 44),
             exitButton.heightAnchor.constraint(equalToConstant: 44)
         ])
+    }
+    // MARK: - Действия
+    @objc private func exitButtonTapped() {
+        dismiss(animated: true)
     }
 }
